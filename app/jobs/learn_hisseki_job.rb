f72ecb6ad1ecac3509469a962bb20f63059da6dd
takuminmin-m <takuminmin0718@gmail.com>
2021-06-13 17:51:52 +0900

    hisseki学習のジョブを作成(未テスト)

--- app/jobs/learn_hisseki_job.rb
+++ app/jobs/learn_hisseki_job.rb
@@ -1,9 +1,9 @@
 class LearnHissekiJob < ApplicationJob
   queue_as :default
+  require 'pycall/import'
+  include PyCall::Import
 
-  def perform(*args)
-    # pythonライブラリのインポート
-    include PyCall
+  def perform
     pyimport :tensorflow, as: :tf
     pyfrom "tensorflow.keras", import: [:datasets, :layers, :models, :optimizers]
     pyimport :numpy, as: :np
@@ -11,11 +11,43 @@ class LearnHissekiJob < ApplicationJob
 
     #ファイルを収集
     datas = Hisseki.all
-    learn_hissekis = datas.map { |e| e.image.current_path }
-    learn_labels = datas.map { |e| e.user_id }
+    hisseki_image_paths = datas.map { |hisseki| hisseki.image.current_path }
+    hisseki_images = read_images(hisseki_image_paths)
+    hisseki_images.map! { |image| tf.cast(image, tf.float32) / 255.0 }
+    hissekis = datas.map_with_index do |hisseki, i|
+      {
+        label: hisseki.user_id,
+        image: hisseki_images[i]
+      }
+    end
+    hissekis.shuffle!
+    puts "LearnHissekiJob: loaded hissekis"
 
-    p learn_hissekis
-    p learn_labels
+    learn_images = np.array hissekis.map { |hisseki| hisseki.image }
+    learn_labels = np.array hissekis.map { |hisseki| hisseki.label }
 
+    model = models.Sequential.new([
+      layers.Conv2D.new(64, [5, 5], activation: :relu, input_shape: [118, 128, 1]),
+      layers.MaxPooling2D.new([4, 4]),
+      layers.Conv2D.new(64, [5, 5], activation: :relu),
+      layers.MaxPooling2D.new([4, 4]),
+      layers.Conv2D.new(32, [5, 5], activation: :relu),
+      layers.Flatten.new,
+      layers.Dense.new(128, activation: :relu),
+      layers.Dense.new(members.length, activation: :softmax)
+    ])
+
+    adam = tf.keras.optimizers.Adam.new(
+      learning_rate: 0.001,
+    )
+
+    model.compile(
+      optimizer: adam,
+      loss: :sparse_categorical_crossentropy,
+      metrics: [:accuracy]
+    )
+    model.fit learn_images, learn_labels, epochs: 20
+
+    # model.save("models/tf/#{t_str}")
   end
 end
