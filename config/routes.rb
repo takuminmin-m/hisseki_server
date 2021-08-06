92e93d9bdaad9bbb218dceb4fd9d9cb7b314df19
takuminmin-m <takuminmin0718@gmail.com>
2021-08-06 14:57:33 +0900

    scaffoldで生成したユーザーを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   resources :hissekis, only: [:new, :create, :index]
   root "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
