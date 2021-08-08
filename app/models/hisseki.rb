41e8500b2a015c234493d0e85d2022bf03899a56
takuminmin-m <takuminmin0718@gmail.com>
2021-08-08 21:50:50 +0900

    絶対パスでファイル位置を指定

--- app/models/hisseki.rb
+++ app/models/hisseki.rb
@@ -5,7 +5,7 @@ class Hisseki < ApplicationRecord
   validates :image, presence: true
 
   def hisseki_size
-    @minimagick_image = MiniMagick::Image.open("public#{image}")
+    @minimagick_image = MiniMagick::Image.open(Rails.root.join("public#{image}"))
     errors.add(:base, "画像のサイズが異なります") unless (@minimagick_image.type == "PNG") && (@minimagick_image.dimensions == [128, 128])
   end
 end
