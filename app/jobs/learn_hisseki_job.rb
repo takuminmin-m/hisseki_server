d7340ee615845f12855bb2594d0fe5d62eec6f94
takuminmin-m <takuminmin0718@gmail.com>
2021-08-11 12:09:01 +0900

    LearnHissekiJobを整頓

--- app/jobs/learn_hisseki_job.rb
+++ app/jobs/learn_hisseki_job.rb
@@ -7,7 +7,16 @@ class LearnHissekiJob < ApplicationJob
     pyimport :numpy, as: :np
     pyimport "matplotlib.pyplot", as: :plt
 
-    #ファイルを収集
+    hisseki_datas = set_datas
+
+    make_classification_model(hisseki_datas).save("ml/hisseki_classification.tf")
+    puts "LearnHissekiJob: saved classification model"
+  end
+
+  private
+
+  # 画像ファイルをすべて読み込む
+  def set_datas
     datas = Hisseki.all
     hisseki_image_paths = datas.map { |hisseki| hisseki.image.current_path }
     hisseki_images = read_images(hisseki_image_paths)
@@ -18,15 +27,18 @@ class LearnHissekiJob < ApplicationJob
         image: hisseki_images[i]
       }
     end
-    hissekis.shuffle!
     puts "LearnHissekiJob: loaded hissekis"
+    hissekis.shuffle!
+  end
 
-    learn_images = np.array hissekis.map { |hisseki| hisseki[:image] }
-    learn_labels = np.array hissekis.map { |hisseki| hisseki[:label] }
+  # ユーザー分類用モデルを作る
+  def make_classification_model(learn_datas)
+    learn_images = np.array learn_datas.map { |hisseki| hisseki[:image] }
+    learn_labels = np.array learn_datas.map { |hisseki| hisseki[:label] }
 
-    member_num = hissekis.map { |hisseki| hisseki[:label] }.uniq.length + 1
-    puts "member_num: #{member_num}"
+    member_num = learn_datas.map { |hisseki| hisseki[:label] }.uniq.length + 1
 
+    puts "LearnHissekiJob: start to make classification model"
     model = models.Sequential.new([
       layers.Conv2D.new(64, [5, 5], activation: :relu, input_shape: [128, 128, 1]),
       layers.MaxPooling2D.new([4, 4]),
@@ -47,11 +59,10 @@ class LearnHissekiJob < ApplicationJob
       loss: :sparse_categorical_crossentropy,
       metrics: [:accuracy]
     )
+
     model.fit learn_images, learn_labels, epochs: 20
 
-    model.save("ml/hisseki_classification.tf")
+    puts "LearnHissekiJob: finish making classification model"
+    model
   end
-
-
-
 end
