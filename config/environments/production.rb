705a9ae78f240edb4ea9a1d4757874fc402c6b20
takuminmin-m <takuminmin0718@gmail.com>
2021-08-27 21:25:47 +0900

    デザインの調整

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # 古い画像のキャッシュを回避
+  config.assets.digest = true
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
