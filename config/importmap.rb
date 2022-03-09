b312b476344a84e3d807fc0998c2d995be06c707
takuminmin-m <takuminmin0718@gmail.com>
2022-03-09 22:04:53 +0900

    remove node

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,5 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.esm.min.js"
 pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.2/dist/esm/index.js"
